9cd52845ad0515dcfa7c536a50916dac3f0ccba6
elizabeth1331 <44823989+elizabeth1331@users.noreply.github.com>
2021-02-01 23:47:51 -0600

    Propuestas de las vistas

---
+++ s-08-vistas.sql
@@ -0,0 +1,36 @@
+--@Autor(es): GARCIA MENESES JEREMY, MENDOZA DE LA VEGA DULCE ELIZABETH
+--@Fecha creación: 01/02/2021
+--@Descripción: CREACION TABLAS DEL CASO DE ESTUDIO
+
+
+--En la empresa se implemento el manejo de vitas para controlar el acceso a algunos datos de
+--los usuarios, mostrando el mes y año de expedición de la tarjeta de cada uno de los clientes
+--esto con la finalidad de crear un registro de dichos usuarios con tarjetas expiradas
+
+create or replace view v_usuario(
+nombre_usuario, email, anio_exp,mes_exp
+) as select nombre_usuario, email, anio_exp,mes_exp
+from usuario u, tarjeta_credito tc
+where u.usuario_id=tc.usuario_id;
+
+
+
+--Vista usada para que los clientes vean las calificaciones y comenatarios de los clientes
+--que alquilaron sus casas para vacacionar 
+
+create or replace view v_vivienda_calificacion_arrendatario(
+folio,vivienda_id,calificacion,descripcion, fecha
+) as select folio,cv.vivienda_id, calificacion,descripcion, fecha
+from usuario u, calificacion_vivienda cv, alquiler a
+where u.usuario_id=cv.usuario_id
+and a.vivienda_id=cv.vivienda_id;
+
+--vista para los usuarios busquen alquilar una casa puedan ver la reseñas de las casas a qluilar, ademas de datos de lugar
+
+create or replace view v_vivienda_calificacion_cliente(
+nombre_usuario,vivienda_id, calificacion, fecha, direccion, capacidad_max,costo_dia
+) as select nombre_usuario,v.vivienda_id, calificacion, fecha, direccion, capacidad_max,costo_dia
+from usuario u, calificacion_vivienda cv,  vivienda v, vivienda_vacacionar vv
+where u.usuario_id=cv.usuario_id
+and v.vivienda_id=vv.vivienda_id
+and u.usuario_id=v.usuario_id;
